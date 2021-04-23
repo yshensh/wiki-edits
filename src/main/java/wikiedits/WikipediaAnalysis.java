a7401ba8754e8325c2dc8b8069f39aa78fd30d6e
Yi Shen <yshensh@gmail.com>
2021-04-22 20:30:58 -0400

    complete sample flink app

---
+++ src/main/java/wikiedits/WikipediaAnalysis.java
@@ -0,0 +1,48 @@
+package wikiedits;
+
+import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+
+public class WikipediaAnalysis {
+    public static void main(String[] args) throws Exception {
+        // set execution parameters
+        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // create sources for reading from external systems
+        DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
+
+        // create DataStream of WikipediaEditEvent elements
+        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
+                .keyBy(new KeySelector<WikipediaEditEvent, String>() {
+                    @Override
+                    public String getKey(WikipediaEditEvent event){
+                        return event.getUser();
+                    }
+                });
+
+        // aggregate the sum of edited bytes for every five seconds
+        DataStream<Tuple2<String, Long>> result = keyedEdits
+                .timeWindow(Time.seconds(5))
+                .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) {
+                        acc.f0 = event.getUser();
+                        acc.f1 += event.getByteDiff();
+                        return acc;
+                    }
+                });
+
+        // print the stream to the console and start execution
+        result.print();
+
+        // start the Flink job
+        see.execute();
+    }
+}
